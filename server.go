e170db8ec5f36be1b2f5a47fb0a049b08cbcca98
iLoveRamona <iLoveRamona>
2025-06-14 01:31:41 +0300

    Full menu functionality

--- server.go
+++ server.go
@@ -17,12 +17,12 @@ func displayMenu() string {
 0 - Меню
 1 - Create
 2 - Read
-3 - Search
-4 - Update
-5 - Delete
+3 - Update
+4 - Delete
 exit - Выйти
 `
 }
+
 func displayCreateMenu() string {
 	return `
  ------------------
@@ -30,11 +30,70 @@ func displayCreateMenu() string {
  ------------------
 1/
 |---- 0 - Меню
-|---- 1 - Сгенерировать книгу
-|---- 2 - Ввести книгу вручную
+|---- 1 - Ввести книгу
 |---- exit - Назад
 `
 }
+
+func displayReadMenu() string {
+	return `
+ ---------------
+| Просмотр книг |
+ ---------------
+2/
+|---- 0 - Меню
+|---- 1 - Вывести книги
+|---- exit -  Назад
+`
+}
+
+func displayFilterMenu() string {
+	return `
+ -------------
+| Найти книги |
+ -------------
+---- */
+|---- ---- 0 - Меню
+|---- ---- 1 - По полю 'id'
+|---- ---- 2 - По полю 'name'
+|---- ---- 3 - По полю 'year'
+|---- ---- 4 - По полю 'authors'
+|---- ---- 5 - По полю 'genres'
+|---- ---- 6 - По полю 'width'
+|---- ---- 7 - По полю 'height'
+|---- ---- 8 - По полю 'cover'
+|---- ---- 9 - По полю 'source'
+|---- ---- 10 - По полю 'added'
+|---- ---- 11 - По полю 'read'
+|---- ---- 12 - По полю 'rating'
+|---- ---- exit -  Назад
+`
+}
+
+func displayUpdateMenu() string {
+	return `
+ ---------------
+| Обновить книги |
+ ---------------
+3/
+|---- 0 - Меню
+|---- 1 - Обновить книги
+|---- exit -  Назад
+`
+}
+
+func displayDeleteMenu() string {
+	return `
+---------------
+| Удалить книги |
+ ---------------
+4/
+|---- 0 - Меню
+|---- 1 - Удалить книги
+|---- exit -  Назад
+`
+}
+
 func main() {
 	listener, err := net.Listen("tcp", port)
 	if err != nil {
@@ -95,18 +154,71 @@ func handleClient(conn net.Conn) {
 				case "0":
 					sendMessage(displayCreateMenu())
 				case "1":
-					sendMessage("Генерация книги... ")
-					sendMessage("Книга сгенерирована. Отправьте '0' для возвращения в меню")
-				case "2":
-					sendMessage("Ручной ввод книги... ")
+					sendMessage("Добавление книги... ")
 					sendMessage("Книга добавлена. Отправьте '0' для возвращения в меню")
+				default:
+					sendMessage("Неверный выбор в подменю. Попробуйте снова.")
+				}
+			}
+		case "2":
+			sendMessage(displayReadMenu())
+		readLoop:
+			for scanner.Scan() {
+				subText := strings.TrimSpace(scanner.Text())
+				switch subText {
+				case "exit":
+					sendMessage("Возврат в главное меню")
+					sendMessage(displayMenu())
+					break readLoop
+				case "0":
+					sendMessage(displayReadMenu())
+				case "1":
+					sendMessage("Вывод всех книг...")
+					sendMessage("Книги выведены. Отправьте '0' для возвращения в меню")
+				default:
+					sendMessage("Неверный выбор в подменю. Попробуйте снова.")
+				}
+			}
+		case "3":
+			sendMessage(displayUpdateMenu())
+		updateLoop:
+			for scanner.Scan() {
+				subText := strings.TrimSpace(scanner.Text())
+				switch subText {
+				case "exit":
+					sendMessage("Возврат в главное меню")
+					sendMessage(displayMenu())
+					break updateLoop
+				case "0":
+					sendMessage(displayUpdateMenu())
+				case "1":
+					sendMessage("Обновление книг...")
+					sendMessage("Книги обновлены. Отправьте '0' для возвращения в меню")
+				default:
+					sendMessage("Неверный выбор в подменю. Попробуйте снова.")
+				}
+			}
+		case "4":
+			sendMessage(displayDeleteMenu())
+		deleteLoop:
+			for scanner.Scan() {
+				subText := strings.TrimSpace(scanner.Text())
+				switch subText {
+				case "exit":
+					sendMessage("Возврат в главное меню")
+					sendMessage(displayMenu())
+					break deleteLoop
+				case "0":
+					sendMessage(displayDeleteMenu())
+				case "1":
+					sendMessage("Удаление книг...")
+					sendMessage("Книги удалены. Отправьте '0' для возвращения в меню")
 				default:
 					sendMessage("Неверный выбор в подменю. Попробуйте снова.")
 				}
 			}
 		default:
 			sendMessage("Неверный выбор. Попробуйте снова.")
-			sendMessage(displayCreateMenu())
 		}
 	}
 	if err := scanner.Err(); err != nil {
